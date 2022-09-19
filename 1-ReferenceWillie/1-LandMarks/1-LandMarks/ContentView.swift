3470b458ff7bf9acc0f1075b7fed1f7cc28c5ab9
yangtao <940463731@qq.com>
2022-09-19 18:01:02 +0800

    测试

---
+++ 1-ReferenceWillie/1-LandMarks/1-LandMarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  1-LandMarks
+//
+//  Created by Mr Sheep on 2022/9/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 1-ReferenceWillie/1-LandMarks/1-LandMarks/__LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  __LandMarksApp.swift
+//  1-LandMarks
+//
+//  Created by Mr Sheep on 2022/9/19.
+//
+
+import SwiftUI
+
+@main
+struct __LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
